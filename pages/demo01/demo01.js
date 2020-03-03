9a80e74f2416f7433b73e6d14ab4a2dadcf04084
yq2020 <61315135+yq2020@users.noreply.github.com>
2020-03-03 16:24:39 +0800

    Add files via upload

---
+++ pages/demo01/demo01.js
@@ -0,0 +1,38 @@
+// pages/demo01/demo01.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:"hello wrold",
+    
+    num:100000,
+    
+    torf:true,
+    
+    person:{
+      age:74,
+      height:145,
+      weigth:200,
+      name:"富婆"
+    },
+    
+    isChecked:true,
+
+    list:[
+      {
+        id:0,
+        name:"猪八戒"
+      },
+      {
+        id:1,
+        name:"天蓬元帅"
+      },
+      {
+        id:2,
+        name:"悟能"
+      }
+    ]
+  }
+})
